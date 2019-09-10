e46854f40d87df98299a9a0034f675248fa161a2
Gerson Espinal <gerson.espinal@gmail.com>
2019-09-10 11:31:51 -0400

    Add authorized routes and restructured codebase

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "this is the scream body",
+      createdAt: "2019-09-05T19:32:55.237Z",
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apiKey: "AIzaSyCobqEob2J90A_yL-QVp3dKgofZQiHvmpU",
+  authDomain: "social-794ae.firebaseapp.com",
+  databaseURL: "https://social-794ae.firebaseio.com",
+  projectId: "social-794ae",
+  storageBucket: "social-794ae.appspot.com",
+  messagingSenderId: "610288904124",
+  appId: "1:610288904124:web:07f777693cf2266a0fadb9"
+};
